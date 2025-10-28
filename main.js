b8ef6b797916c3925c189e225587cbb79968baf3
Adebayo Adewusi <adewusiadebayo@gmail.com>
2025-10-28 17:17:04 +0100

    Text more functions

--- main.js
+++ main.js
@@ -9,11 +9,22 @@ list.append("hamster");
 list.append("snake");
 list.append("turtle");
 
+console.log("Initial list:");
 console.log(list.toString());
 
 list.prepend("goat");
+console.log("\nAfter prepending 'goat':");
 console.log(list.toString());
-console.log(`The total number of list is: ${list.size()}`);
-console.log(`The List head is: ${list.head}`);
-console.log(`The List tail is: ${list.getTail()}`);
-console.log(`The node at index 2 is: ${list.at(2)}`);
+
+console.log(`\nThe total number of nodes is: ${list.size()}`);
+console.log(`The List head is: ${list.getHead().value}`);
+console.log(`The List tail is: ${list.getTail().value}`);
+console.log(`The node at index 2 is: ${list.at(2).value}`);
+console.log(`The node at index 2 is: ${list.pop()}`);
+
+console.log(`\nAfter popping the list:`);
+console.log(list.toString());
+
+console.log(`\nThe list contains 'dog': ${list.contains("dog")}`);
+console.log(`The list contains 'cow': ${list.contains("cow")}`);
+console.log(`The index of parrot is: ${list.find("parrot")}"`);
