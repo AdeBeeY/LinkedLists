545f561af9499f941750127b64ac36851192fdd6
Adebayo Adewusi <adewusiadebayo@gmail.com>
2025-10-28 16:44:37 +0100

    test the functions with examples

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
-import { Node, LinkedList } from './linkedList';
+import { Node, LinkedList } from './linkedList.js';
 
+const list = new LinkedList();
+
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
+
+console.log(list.toString());
+
+list.prepend("goat");
+console.log(list.toString());
+console.log(`The total number of list is: ${list.size()}`);
+console.log(`The List head is: ${list.head}`);
+console.log(`The List tail is: ${list.getTail()}`);
+console.log(`The node at index 2 is: ${list.at(2)}`);
